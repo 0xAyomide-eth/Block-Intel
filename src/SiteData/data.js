04f2ab1881c5b7bec3833820be312d67e183e94e
0xAyomide-eth <xayomide.sol@gmail.com>
2026-03-04 23:42:35 +0100

    changes made

---
+++ src/SiteData/data.js
@@ -0,0 +1,46 @@
+import AIChatIcon from "../assets/icons/greenishicons/icons8-ai-90.png"
+import TradingIcon from "../assets/icons/greenishicons/icons8-trading-50.png"
+import NewsIcon from "../assets/icons/greenishicons/icons8-news-50.png"
+import RatingIcon from "../assets/icons/greenishicons/icons8-rating-64.png"
+import AlertsIcon from "../assets/icons/greenishicons/icons8-google-alerts-50.png"
+import AveragePriceIcon from "../assets/icons/greenishicons/icons8-average-price-50.png"
+
+
+export default [
+    {
+        id: 1,
+        image: AIChatIcon,
+        title:"AI chat mode",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    },
+    {
+        id: 2,
+        image: TradingIcon,
+        title:"Trading",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    },
+    {
+        id: 3,
+        image: NewsIcon,
+        title:"News",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    }, 
+    {
+        id: 4,
+        image: RatingIcon,
+        title:"Rating",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    },
+    {
+        id: 5,
+        image: AlertsIcon,
+        title:"Alerts",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    },  
+    {
+        id: 6,
+        image: AveragePriceIcon,
+        title:"AveragePrice",
+        content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit sint, ad odio est perferendis provident nemo dolor et cumque quidem."
+    },                     
+]
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,11 @@
 import arrowIcon from '../assets/icons/whiteicons/icons8-arrow-50.png'
-import './style.css'
+import '../App.css'
 
 export default function Card(props) {
 return(
         <div className="box">
-        <img src={props.img} alt="averagePrice-icon" className='box-pic' />
-        <p className='box-title'>{props.text}</p>
+        <img src={props.image} alt="averagePrice-icon" className='box-pic' />
+        <p className='box-title'>{props.title}</p>
         <p>{props.content}</p>
         <button>Check Out <img src={arrowIcon} alt="" className='arrow' /></button>
     </div>
